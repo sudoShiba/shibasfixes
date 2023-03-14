584256b60eb8f69601647afe1e4502521da42923
Shiba <robzun1000@gmail.com>
2023-03-14 09:09:34 +0100

    adding css using manifest.json instead of through javascript

--- scripts/smartschool.js
+++ scripts/smartschool.js
@@ -1,9 +1,3 @@
-// adding css
-const link = document.createElement("link")
-link.setAttribute("rel", "stylesheet");
-link.setAttribute("href","moz-moz-extension://8c49c01c-b79e-41a0-a200-bbc98172ece9/css/ss.css");
-document.head.appendChild(link);
-
 // date
 const date = new Date;
 const datetext = document.createElement("a");
